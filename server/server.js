b9ae23f7e991409a2cb9cee73f89946442bd92e2
im-nihar <niharsonawane@gmail.com>
2024-08-12 18:59:10 +0100

    [WIP] Minor date changes

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ const getFormattedDateTime = () => {
 // Function to handle sending email notifications
 const handleEmail = (planeData) => {
   // const transporter = nodemailer.createTransport(EMAIL_CONFIG);
-  const timeStamp = moment().format('D-MM-YYYY, h:mm:ss a'); //getFormattedDateTime();
+  const timeStamp = moment(1000*60*60*1).utc().format('D-MM-YYYY, h:mm:ss a'); //getFormattedDateTime();
   const planeWord = planeData.length === 1 ? "Plane" : "Planes";
   const subject = `[${timeStamp}] New ${planeWord} above you: ${planeData.length}!`;
   const body = planeData
